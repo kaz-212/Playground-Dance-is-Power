8684aa699318097b8fccaef28ecc40198baf24c7
Karan Basu <karan.basu16@gmail.com>
2024-05-13 21:46:09 +0100

    feat: initialise dancing girl

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <main>
+    <h1>Dance is Power</h1>
+    <Spline scene="https://prod.spline.design/nbNul2bDeW3SRbn0/scene.splinecode" />
+    </main>
+  );
+}
+
