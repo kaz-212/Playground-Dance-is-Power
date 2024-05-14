b26a482e09344a15c6ff92c7522c6dc18cb93f41
Karan Basu <karan.basu16@gmail.com>
2024-05-14 20:47:16 +0100

    style: restyle the cursor

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ export default function App() {
     const currentWord = NOUNS[wordIndex % 4];
     for (let i = 0; i <= currentWord.length; i++) {
       await sleep(30 + Math.random() * 400);
-      console.log(currentWord.slice(0, i));
+      // console.log(currentWord.slice(0, i));
       nounRef.current.innerText = currentWord.slice(0, i);
     }
     await sleep(1200);
     for (let i = currentWord.length; i >= 0; i--) {
       await sleep(80);
-      console.log(currentWord.slice(0, i));
+      // console.log(currentWord.slice(0, i));
       nounRef.current.innerText = currentWord.slice(0, i);
     }
     setWordIndex(wordIndex + 1);
@@ -28,8 +28,14 @@ export default function App() {
     typeWord(wordIndex);
   }, [wordIndex]);
 
+  const cursorRef = useRef(null);
+  const onMouseMove = e => {
+    cursorRef.current.style.transform = `translate3D(${e.clientX}px, ${e.clientY}px, 0)`;
+  };
+
   return (
-    <main>
+    <main onMouseMove={onMouseMove}>
+      <div className='custom-cursor' ref={cursorRef}></div>
       <h1>
         Dance is <span className='noun' ref={nounRef}></span>
       </h1>
